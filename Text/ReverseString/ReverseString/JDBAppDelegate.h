cac8894ccf707dd4318cfb5aa12683269ef2bc6e
jbranchaud <jbranchaud@gmail.com>
2014-04-28 19:19:31 -0300

    Initial commit for the ReverseString project.

---
+++ Text/ReverseString/ReverseString/JDBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JDBAppDelegate.h
+//  ReverseString
+//
+//  Created by Josh Branchaud on 4/28/14.
+//  Copyright (c) 2014 ios-projects. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JDBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Text/ReverseString/ReverseString/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ReverseString
+//
+//  Created by Josh Branchaud on 4/28/14.
+//  Copyright (c) 2014 ios-projects. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JDBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JDBAppDelegate class]));
+    }
+}
