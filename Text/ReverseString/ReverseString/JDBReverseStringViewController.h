d303b24f66db4485d6c23ccd7ffdd63750b5a175
jbranchaud <jbranchaud@gmail.com>
2014-04-28 20:26:53 -0300

    Add a custom UIViewController, JDBReverseStringViewController, and set it as the class for the ViewController in the storyboard.

---
+++ Text/ReverseString/ReverseString/JDBReverseStringViewController.h
@@ -0,0 +1,13 @@
+//
+//  JDBReverseStringViewController.h
+//  ReverseString
+//
+//  Created by Josh Branchaud on 4/28/14.
+//  Copyright (c) 2014 ios-projects. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JDBReverseStringViewController : UIViewController
+
+@end
---
+++ Text/ReverseString/ReverseString/JDBReverseStringViewController.m
@@ -0,0 +1,49 @@
+//
+//  JDBReverseStringViewController.m
+//  ReverseString
+//
+//  Created by Josh Branchaud on 4/28/14.
+//  Copyright (c) 2014 ios-projects. All rights reserved.
+//
+
+#import "JDBReverseStringViewController.h"
+
+@interface JDBReverseStringViewController ()
+
+@end
+
+@implementation JDBReverseStringViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
