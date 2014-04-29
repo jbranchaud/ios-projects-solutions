05203cb60a91d3acfc4505a037b4afe1565c48e5
jbranchaud <jbranchaud@gmail.com>
2014-04-29 18:24:11 -0300

    Add a view controller subclass for the single view in the storyboard.

---
+++ Text/Palindrome/Palindrome/JDBPalindromeViewController.h
@@ -0,0 +1,13 @@
+//
+//  JDBPalindromeViewController.h
+//  Palindrome
+//
+//  Created by Josh Branchaud on 4/29/14.
+//  Copyright (c) 2014 ios-projects. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JDBPalindromeViewController : UIViewController
+
+@end
