5084d32655ba5f50808b590c3280cfdc56e15e58
jbranchaud <jbranchaud@gmail.com>
2014-04-29 17:12:00 -0300

    Initial commit for the Palindrome project.

---
+++ Text/Palindrome/Palindrome/Palindrome/JDBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JDBAppDelegate.h
+//  Palindrome
+//
+//  Created by Josh Branchaud on 4/29/14.
+//  Copyright (c) 2014 ios-projects. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JDBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
