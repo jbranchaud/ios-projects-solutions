efba70d63d2cb9a99dcc803e638aec30359ea11e
jbranchaud <jbranchaud@gmail.com>
2014-05-08 00:25:13 -0300

    Customize the text fields delegate so that I can control its behavior, specifically, I want to resign the first responder when DONE is clicked.

--- Text/Palindrome/Palindrome/JDBPalindromeViewController.h
+++ Text/Palindrome/Palindrome/JDBPalindromeViewController.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface JDBPalindromeViewController : UIViewController
+@interface JDBPalindromeViewController : UIViewController <UITextFieldDelegate>
 
 - (BOOL)isPalindrome:(NSString *)string;
 
