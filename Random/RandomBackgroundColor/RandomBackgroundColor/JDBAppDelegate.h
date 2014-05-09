40968ce3bd9ec0699c5d8ec2bde3032593f7cb3c
jbranchaud <jbranchaud@gmail.com>
2014-05-09 09:07:03 -0300

    Initial commit for the RandomBackgroundColor project.

---
+++ Random/RandomBackgroundColor/RandomBackgroundColor/JDBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JDBAppDelegate.h
+//  RandomBackgroundColor
+//
+//  Created by Josh Branchaud on 5/9/14.
+//  Copyright (c) 2014 ios-projects. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JDBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Random/RandomBackgroundColor/RandomBackgroundColor/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RandomBackgroundColor
+//
+//  Created by Josh Branchaud on 5/9/14.
+//  Copyright (c) 2014 ios-projects. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JDBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JDBAppDelegate class]));
+    }
+}
