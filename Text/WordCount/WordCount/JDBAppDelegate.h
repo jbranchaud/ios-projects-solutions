bb0dbe856b82cd376cb52aa81cfc8b79a8b54696
jbranchaud <jbranchaud@gmail.com>
2014-05-18 15:38:31 -0300

    Initial commit for the Word Count project.

---
+++ Text/WordCount/WordCount/JDBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JDBAppDelegate.h
+//  WordCount
+//
+//  Created by Josh Branchaud on 5/18/14.
+//  Copyright (c) 2014 ios-projects. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JDBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Text/WordCount/WordCount/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WordCount
+//
+//  Created by Josh Branchaud on 5/18/14.
+//  Copyright (c) 2014 ios-projects. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JDBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JDBAppDelegate class]));
+    }
+}
