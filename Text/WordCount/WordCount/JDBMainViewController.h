55c3eefa2ee7afbab2cdaba3538a6d6010ffb3d2
jbranchaud <jbranchaud@gmail.com>
2014-05-18 16:05:00 -0300

    Add a custom view controller to replace the default one.

---
+++ Text/WordCount/WordCount/JDBMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  JDBMainViewController.h
+//  WordCount
+//
+//  Created by Josh Branchaud on 5/18/14.
+//  Copyright (c) 2014 ios-projects. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JDBMainViewController : UIViewController
+
+@end
