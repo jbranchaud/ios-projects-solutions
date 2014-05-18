3855160b222d796bd05c5a46eb3ff7ca7fde1221
jbranchaud <jbranchaud@gmail.com>
2014-05-18 16:12:02 -0300

    Comment out the default window initialization text, it is now taken care of by the storyboard.

--- Text/WordCount/WordCount/JDBAppDelegate.m
+++ Text/WordCount/WordCount/JDBAppDelegate.m
@@ -7,15 +7,16 @@
 //
 
 #import "JDBAppDelegate.h"
+#import "JDBMainViewController.h"
 
 @implementation JDBAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
-    self.window.backgroundColor = [UIColor whiteColor];
-    [self.window makeKeyAndVisible];
+//    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+//    // Override point for customization after application launch.
+//    self.window.backgroundColor = [UIColor whiteColor];
+//    [self.window makeKeyAndVisible];
     return YES;
 }
 
